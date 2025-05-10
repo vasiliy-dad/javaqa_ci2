9cc3b7a7df5fe188bc8f4db9bf91e9f5aac39a9d
vasiliy-dad <dadvasaki@gmail.com>
2025-05-10 19:18:11 +0800

    Test PostmanApi

---
+++ src/test/java/ru/netology/post/PostmanApiTest.java
@@ -0,0 +1,26 @@
+package ru.netology.post;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanApiTest {
+    @Test
+    void shouldPostOnPostmanEcho() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("{\"args\": \"Vasya\", \"form\": 30}") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .contentType("text/plain; charset=UTF-8")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("{\"args\": \"Vasya\", \"form\": 30}"))
+        ;
+    }
+}
