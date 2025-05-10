29d7054e3a76907fab19eae19c5441176cb849f0
vasiliy-dad <dadvasaki@gmail.com>
2025-05-10 19:26:35 +0800

    Change test to normal

--- src/test/java/ru/netology/post/PostmanApiTest.java
+++ src/test/java/ru/netology/post/PostmanApiTest.java
@@ -20,7 +20,7 @@ public class PostmanApiTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("{\"args\": \"Vasya\", \"form\": 33}"))
+                .body("data", equalTo("{\"args\": \"Vasya\", \"form\": 30}"))
         ;
     }
 }
